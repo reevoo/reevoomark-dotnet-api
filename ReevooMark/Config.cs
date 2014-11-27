b7279ca1a0b38e691e04b1f5b8bbcfe28d2ccf35
Rene Klacan <rene.klacan@reevoo.com>
2014-11-27 20:01:29 +0000

    Fix Config to have a production values

--- ReevooMark/Config.cs
+++ ReevooMark/Config.cs
@@ -4,8 +4,8 @@ namespace ReevooMark {
 
 	public static class Config {
 		public static string protocol = "http";
-		//public static string host = "mark.reevoo.com";
-		public static string host = "localhost:3001";
+		public static string host = "mark.reevoo.com";
+		//public static string host = "localhost:3001";
 
 		public static string BaseUri() {
 			return String.Format("{0}://{1}/", protocol, host);
